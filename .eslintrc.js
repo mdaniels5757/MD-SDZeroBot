a19f07bed3779352b50ed409f2cade4f743d2f3b
Michael Daniels <mdaniels5757@gmail.com>
2025-05-03 15:39:19 -0400

    rm mocha, update nodemon

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,6 @@ module.exports = {
         "commonjs": true,
         "es2021": true,
         "node": true,
-        "mocha": true
     },
     "extends": "eslint:recommended",
     "parserOptions": {
